50ff8c5f09780a4b6d0eac3f43eef30158cea71a
c-ruprecht <constantin.ruprecht@gmail.com>
2024-07-09 10:17:29 -0400

    test deployment

---
+++ apps/app.py
@@ -0,0 +1,19 @@
+bashCopycat << EOF > ~/www/dashapp/app/app.py
+import dash
+from dash import html, dcc
+import plotly.express as px
+
+app = dash.Dash(__name__)
+server = app.server
+
+# Sample layout
+app.layout = html.Div([
+    html.H1('My Dash App'),
+    dcc.Graph(
+        id='example-graph',
+        figure=px.scatter(x=[1, 2, 3], y=[4, 1, 2])
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ index.wsgi
@@ -0,0 +1,4 @@
+import sys
+sys.path.insert(0, '/hpc/users/ruprec01/www/culture_db/app')
+
+from app import server as application
