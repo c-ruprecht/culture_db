36d32e16e6e0eb94589aded60bb15aa7a378f9bc
c-ruprecht <constantin.ruprecht@gmail.com>
2024-07-09 15:00:22 -0400

    minimal0.1

--- culture_db_minimal.py
+++ culture_db_minimal.py
@@ -5,10 +5,7 @@ from dash import dcc, html
 # Initialize the Dash app
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.css"
 exp_btn_css = 'assets/exoort_btn.css'
-app = dash.Dash(__name__,
-                suppress_callback_exceptions=True,
-                prevent_initial_callbacks=True,
-                external_stylesheets=[dbc.themes.LUX, exp_btn_css, dbc_css])
+app = dash.Dash(external_stylesheets=[dbc.themes.LUX, exp_btn_css, dbc_css])
 
 # Simple layout for testing
 app.layout = html.Div(children=[
