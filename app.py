af1ee5176374cff4be4df53a4668619156908f2b
c-ruprecht <constantin.ruprecht@gmail.com>
2024-07-09 15:37:12 -0400

    app

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from apps import home, browse_sql, analysis_donor
 from components.layout import layout
 
 # Initialize the Dash app
-dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.css"
-exp_btn_css = 'components/export_btn.css'
+#dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.css"
+#exp_btn_css = 'components/export_btn.css'
 
 app = dash.Dash(__name__,
-                external_stylesheets=[dbc.themes.LUX, exp_btn_css, dbc_css],
+                external_stylesheets=[dbc.themes.LUX]#, exp_btn_css, dbc_css],
                 prevent_initial_callbacks=True,
                 suppress_callback_exceptions=True)
 # Set the layout for the Dash app
