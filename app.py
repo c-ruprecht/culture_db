9011462be488d04dc799914e72b2c28f4711e4d0
c-ruprecht <constantin.ruprecht@gmail.com>
2024-07-16 17:32:16 -0400

    still fixing imports

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import dash
 import dash_bootstrap_components as dbc
 import dash_html_components as html
-from dash import dcc
+import dash_core_compenents as dcc 
 from navbar import navbar, CONTENT_STYLE
 
 # For deployment, pass app.server (which is the actual flask app) to WSGI etc
