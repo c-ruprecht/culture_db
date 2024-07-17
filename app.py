df95614a450673c4c07cae78134888ba288a9b0e
c-ruprecht <constantin.ruprecht@gmail.com>
2024-07-17 16:40:46 -0400

    classic navbar

--- app.py
+++ app.py
@@ -5,25 +5,37 @@ import dash_core_components as dcc
 from navbar import navbar, CONTENT_STYLE
 from dash.dependencies import Input, Output, State
 from dash import callback
+from pages import home, browse_sql, analysis_donor
+
 
 # For deployment, pass app.server (which is the actual flask app) to WSGI etc
 app = dash.Dash(__name__,
                 external_stylesheets=[dbc.themes.BOOTSTRAP], 
-                requests_pathname_prefix='/culture_db/index.wsgi/',
-                use_pages=True)
+                requests_pathname_prefix='/culture_db/index.wsgi/')
 
 
 app.layout = html.Div([navbar,
-                        html.H1('Multi-page app with Dash Pages'),
-                        html.Div([
-                            html.Div(
-                                dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
-                            ) for page in dash.page_registry.values()
-                        ]),
-                        dash.page_container
+                        dcc.Location(id='url', refresh=False),
+                        html.Div(id='page-content', style=CONTENT_STYLE),
+                        dcc.Store(id='store-db-path', storage_type='local', data={'db_path': "data/culture_db/culture.db"})
                     ])
 
 
+
+@app.callback(
+    Output('page-content', 'children'),
+    Input('url', 'pathname'),
+    State('store-db-path', 'data')
+)
+def display_page(pathname, db_path):
+    if pathname == '/':
+        return home.layout
+    elif pathname == '/culture_db/index.wsgi/analysis_donor':
+        return analysis_donor.layout
+    elif pathname == '/culture_db/index.wsgi/browse_sql':
+        return browse_sql.layout
+    else:
+        return home.layout
 
 if __name__ == '__main__':
     app.run_server(debug=True)
