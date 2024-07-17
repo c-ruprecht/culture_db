749777f071b9b6591a7595be03f70ae3eda454f0
c-ruprecht <constantin.ruprecht@gmail.com>
2024-07-17 14:54:01 -0400

    multipage

--- app.py
+++ app.py
@@ -5,14 +5,25 @@ import dash_core_components as dcc
 from navbar import navbar, CONTENT_STYLE
 
 # For deployment, pass app.server (which is the actual flask app) to WSGI etc
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], requests_pathname_prefix='/culture_db/index.wsgi/',
+app = dash.Dash(__name__,
+                external_stylesheets=[dbc.themes.BOOTSTRAP], 
+                requests_pathname_prefix='/culture_db/index.wsgi/',
                 use_pages=True)
 
-app.layout = html.Div([dash.page_container,
-                        navbar,
-                        dcc.Location(id='url', refresh=False),
-                        html.Div(id='page-content', style=CONTENT_STYLE),
-                        dcc.Store(id='store-db-path', storage_type='local', data={'db_path': "data/culture_db/culture.db"})])
+app.layout = html.Div([html.H1('Multi-page app with Dash Pages'),
+                        html.Div([
+                            html.Div(
+                                dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
+                            ) for page in dash.page_registry.values()
+                        ]),
+                        dash.page_container
+                    ])
+
+#app.layout = html.Div([dash.page_container,
+#                        navbar,
+#                        dcc.Location(id='url', refresh=False),
+#                        html.Div(id='page-content', style=CONTENT_STYLE),
+#                        dcc.Store(id='store-db-path', storage_type='local', data={'db_path': "data/culture_db/culture.db"})])
 
 if __name__ == "__main__":
     app.run_server(port=8050)
