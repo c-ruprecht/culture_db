0827227bf1b0779a03d8349098e7437587d8c8bd
c-ruprecht <constantin.ruprecht@gmail.com>
2024-07-17 15:18:32 -0400

    navigation

--- app.py
+++ app.py
@@ -10,20 +10,27 @@ app = dash.Dash(__name__,
                 requests_pathname_prefix='/culture_db/index.wsgi/',
                 use_pages=True)
 
-#app.layout = html.Div([html.H1('Multi-page app with Dash Pages'),
-#                        html.Div([
-#                            html.Div(
-#                                dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
-#                            ) for page in dash.page_registry.values()
-#                        ]),
-#                        dash.page_container
-#                    ])
-
 app.layout = html.Div([dash.page_container,
                         navbar,
                         dcc.Location(id='url', refresh=False),
                         html.Div(id='page-content', style=CONTENT_STYLE),
                         dcc.Store(id='store-db-path', storage_type='local', data={'db_path': "data/culture_db/culture.db"})])
+
+@app.callback(
+    Output('page-content', 'children'),
+    Input('url', 'pathname'),
+    State('store-db-path', 'data')
+)
+def display_page(pathname, db_path):
+    if pathname == '/':
+        return home.layout
+    elif pathname == '/analysis_donor':
+        return analysis_donor.layout
+    elif pathname == '/browse_sql':
+        return browse_sql.layout
+    else:
+        return home.layout
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
