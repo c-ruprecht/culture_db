ce3f68c914d0fdc1ab5889f830b6352d05657936
c-ruprecht <constantin.ruprecht@gmail.com>
2024-07-17 16:32:24 -0400

    retry classic navbar

--- navbar.py
+++ navbar.py
@@ -1,5 +1,6 @@
 """
-This app creates a simple navbar layout using the dbc.Navbar component.
+This app creates a simple sidebar layout using inline style arguments and the
+dbc.Nav component.
 
 dcc.Location is used to track the current location, and a callback uses the
 current location to render the appropriate page content. The active prop of
@@ -7,76 +8,81 @@ each NavLink is set automatically according to the current pathname. To use
 this feature you must install dash-bootstrap-components >= 0.11.0.
 
 For more details on building multi-page Dash applications, check out the Dash
-documentation: https://dash.plotly.com/urls
+documentation: https://dash.plot.ly/urls
 """
+
+#Usefull links for creatign page structure https://dash.plotly.com/urls
 import dash
 import dash_bootstrap_components as dbc
-import dash_html_components as html
-from dash.dependencies import Input, Output, State
-from dash import callback
+import dash_daq as daq
+from dash import Input, Output, State,  dcc, html, Dash, callback
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash()#external_stylesheets=[dbc.themes.BOOTSTRAP]
 
-# The style arguments for the navbar
+#Replace current sidebar with navbar from
+#https://dash-bootstrap-components.opensource.faculty.ai/docs/components/navbar/
+
+
+# the style arguments for the sidebar. We use position:fixed and a fixed width
 NAVBAR_STYLE = {
     "position": "fixed",
-    "top": "0",
+    "top": "5rem",
     "left": 0,
-    "right": 0,
-    "width": "100%",
-    "padding": "0.5rem 1rem",
-    "background-color": "#343a40",
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
 }
 
-# The styles for the main content position it below the navbar and add some padding
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
 CONTENT_STYLE = {
-    "margin-top": "4rem",
     "margin-left": "2rem",
     "margin-right": "2rem",
     "padding": "2rem 1rem",
 }
 
-import dash
-from dash import html
-import dash_bootstrap_components as dbc
 
-#dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
-#                            ) for page in dash.page_registry.values()
-
-def search_bar():
-    return dbc.Row(
-    [dbc.Col(dbc.Button('Home', href='culture_db/', class_name='btn btn-dark')),
-        dbc.Col(dbc.DropdownMenu([dbc.DropdownMenuItem(f"{page['name']}", href=page["relative_path"]) for page in dash.page_registry.values()],#if page["path"].contains("/analysis")],
-            label='Analysis',
-            color='dark',
-        )),
+search_bar = dbc.Row(
+    [   dbc.Col(dbc.Button('Home', href = '/', class_name= 'btn btn-dark')),
         dbc.Col(dbc.DropdownMenu([
-            dbc.DropdownMenuItem('Custom SQL Query', href='culture_db/browse_sql'),
-        ],
-            label='Browse',
-            color='dark',
-        )),
+                                dbc.DropdownMenuItem('Donor', href = '/analysis_donor'),
+                                ],
+                                label = 'Analysis',
+                                color = 'dark',
+                                )),
+        dbc.Col(dbc.DropdownMenu([
+                                dbc.DropdownMenuItem('Custom SQL Query', href = '/browse_sql'),
+                                ],
+                                label = 'Browse',
+                                color = 'dark',
+                                )),
+        
     ],
     class_name="g-0 ms-auto flex-nowrap mt-3 mt-md-0",
     align="right",
+    #justify="center",
 )
 
 navbar = dbc.Navbar(
     dbc.Container(
         [
             html.A(
+                # Use row and col to control vertical alignment of logo / brand
                 dbc.Row(
                     [
+                        #dbc.Col(html.Img(src=app.get_asset_url('logo_faithlab.png'), height="30px")),
                         dbc.Col(dbc.NavbarBrand("Microbial Culture DB", className="ms-2")),
                     ],
                     align="center",
                     className="g-0",
                 ),
+                #href="https://plotly.com",
                 style={"textDecoration": "none"},
             ),
             dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
             dbc.Collapse(
-                search_bar(),
+                search_bar,
                 id="navbar-collapse",
                 is_open=False,
                 navbar=True,
@@ -84,10 +90,10 @@ navbar = dbc.Navbar(
         ]
     ),
     color="dark",
-    dark=True,
+    dark=True, #True
 )
 
-# Add callback for toggling the collapse on small screens
+# add callback for toggling the collapse on small screens
 @callback(
     Output("navbar-collapse", "is_open"),
     [Input("navbar-toggler", "n_clicks")],
@@ -97,6 +103,8 @@ def toggle_navbar_collapse(n, is_open):
     if n:
         return not is_open
     return is_open
+
+
 
 if __name__ == "__main__":
     app.run_server(port=8888)
