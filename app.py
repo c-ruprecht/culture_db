c376abc4f3d1f69dbba682d709bb9daeb6222abf
c-ruprecht <constantin.ruprecht@gmail.com>
2024-07-17 15:28:12 -0400

    imports

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash_core_components as dcc
 from navbar import navbar, CONTENT_STYLE
+from dash.dependencies import Input, Output, State
+from dash import callback
 
 # For deployment, pass app.server (which is the actual flask app) to WSGI etc
 app = dash.Dash(__name__,
