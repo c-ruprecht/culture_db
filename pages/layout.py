489f08e7228e59edacb52520df467727a821182c
c-ruprecht <constantin.ruprecht@gmail.com>
2024-07-22 13:22:22 -0400

    change db path

--- pages/layout.py
+++ pages/layout.py
@@ -5,5 +5,5 @@ layout = html.Div([
     navbar,
     dcc.Location(id='url', refresh=False),
     html.Div(id='page-content', style=CONTENT_STYLE),
-    dcc.Store(id='store-db-path', storage_type='local', data={'db_path': "data/culture_db/culture.db"})
+    dcc.Store(id='store-db-path', storage_type='local', data={'db_path': "/hpc/users/ruprec01/www/culture_db/data/culture_db/culture.db"})
 ])
